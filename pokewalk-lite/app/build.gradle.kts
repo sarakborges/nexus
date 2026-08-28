4404e837b829e3ecf05a7d5c55325dc050c982d7
Sara Borges <sarakborges@outlook.com>
2026-08-28 07:31:53 -0300

    Bump PokeWalk Lite to V3

--- pokewalk-lite/app/build.gradle.kts
+++ pokewalk-lite/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.pokewalklite"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
     }
 }
 
